4704b01cc200d00c3f7e6a39a2af7b8476e5b8e3
Timbaleek <35731993+Timbaleek@users.noreply.github.com>
2022-11-04 18:13:51 +0100

    Initial commit

---
+++ HPtoGHS.py
@@ -0,0 +1,66 @@
+import requests
+from bs4 import BeautifulSoup
+
+# Made by Timbaleek
+
+wikiLink = input("Deutsche Wikipediaseite: ")
+
+response = requests.get(
+    url=wikiLink,
+)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# title
+#soup.find("span", {"class": "mw-page-title-main"}).get_text()
+title = soup.find("span", {"class": "mw-page-title-main"}).text
+
+#H and P
+#fatherH = soup.find(text="H: ").parent
+#hps = list(fatherH.descendants)
+
+# GHS
+print("\\HPStatements{" + title + "}{\\ghspic{exclam}\\\\\\\\}")
+print("{\\begin{itemize}")
+
+# H
+hs = soup.select("#Vorlage_Infobox_Chemikalie > tbody:nth-child(1) > tr:nth-child(n) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2) > a:nth-child(n) > span:nth-child(1) > span:nth-child(1)")
+
+for h in hs:
+    print("\\item \\ghs{h}{" + h.text + "}")
+
+print("\\end{itemize}}")
+
+# P
+
+print("{\\begin{itemize}")
+
+ps = soup.select("#Vorlage_Infobox_Chemikalie > tbody:nth-child(1) > tr:nth-child(n) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-last-child(1) > td:nth-child(1) > a:nth-child(n) > span:nth-child(1) > span:nth-child(1)")
+
+for p in ps:
+    print("\\item \\ghs{p}{" + p.text + "}")
+
+print("\\end{itemize}}")
+print("{}")
+
+
+# check if hp only contains numbers
+# print(type(hp))
+# if hp.string != None:
+#    if hp.string.isnumeric():
+#        print(hp.string)
+
+#    print(" sdasdasd")
+
+# GHS
+# for hp in hps:
+#    if (hp.get_text() == 'H'):
+#        hp.getChildren().get_text()
+
+#hazards = []
+#precautions = []
+
+#hazards = hazardString.split("-")
+#precautions = precautionString.split("-")
+
+# for hazard in hazards:
+#    print(hazard)
---
+++ HPtoGHSnew.py
@@ -0,0 +1,95 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+# Made by Timbaleek
+
+wikiLink = input("sigmaaldrich.com Webseite: ")
+
+response = requests.get(
+    url=wikiLink, headers={'User-Agent': 'Chrome'}
+)
+
+print(response.url)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# print(soup.prettify())
+
+data = soup.find('script', type='application/json').text
+data = json.loads(data)
+
+print(data)
+
+print(data["props"]["pageProps"]["data"]["getProductDetail"]["name"])
+
+print(data["props"]["pageProps"]["data"]
+      ["getProductDetail"]["compliance"][2]["value"])
+
+# title
+#soup.find("span", {"class": "mw-page-title-main"}).get_text()
+title = data["props"]["pageProps"]["data"]["getProductDetail"]["name"]
+
+
+#H and P
+#fatherH = soup.find(text="H: ").parent
+#hps = list(fatherH.descendants)
+
+# GHS
+print("\\HPStatements{" + title + "}", end="")
+
+# Images #TODO: correct JSON path
+'''ghsImagesJson = data["props"]["pageProps"]["data"]["getProductDetail"]["compliance"][0]["value"]
+imageString = ""
+ghsImages = ghsImagesJson.split(",")
+for image in ghsImages:
+    imageString += "\\ghspic{" + image + "}"'''
+
+print("{\\ghspic{exclam}\\\\\\\\}")
+
+print("{\\begin{itemize}")
+
+# H
+hsString = data["props"]["pageProps"]["data"]["getProductDetail"]["compliance"][2]["value"]
+
+hs = hsString.split(" - ")
+for h in hs:
+
+    print("\\item \\ghs{h}{" + h + "}")
+
+print("\\end{itemize}}")
+
+# P
+
+print("{\\begin{itemize}")
+
+psString = data["props"]["pageProps"]["data"]["getProductDetail"]["compliance"][3]["value"]
+
+ps = psString.split(" - ")
+for p in ps:
+    print("\\item \\ghs{p}{" + p.replace(" ", "") + "}")
+
+print("\\end{itemize}}")
+print("{}")
+
+
+# check if hp only contains numbers
+# print(type(hp))
+# if hp.string != None:
+#    if hp.string.isnumeric():
+#        print(hp.string)
+
+#    print(" sdasdasd")
+
+# GHS
+# for hp in hps:
+#    if (hp.get_text() == 'H'):
+#        hp.getChildren().get_text()
+
+#hazards = []
+#precautions = []
+
+#hazards = hazardString.split("-")
+#precautions = precautionString.split("-")
+
+# for hazard in hazards:
+#    print(hazard)
