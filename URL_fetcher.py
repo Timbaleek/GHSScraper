5241a623c1147059ce03a6462199ec41693e764e
Helusio <103414789+Helusio@users.noreply.github.com>
2022-11-05 00:44:13 +0100

    gestis_data thinning

--- URL_fetcher.py
+++ URL_fetcher.py
@@ -2,5 +2,7 @@ import pandas as pd
 import numpy as np
 
 gestis_data = pd.read_excel('gestis_data.xlsx')
-gestis_data.to_dict()
 print(gestis_data)
+print(gestis_data.loc[gestis_data['FORMULA'] == 'H2O'])
+
+
