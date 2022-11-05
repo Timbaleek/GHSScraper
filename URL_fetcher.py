4d1b124d9da0bad68ed22b07082055f9fd1bde4b
Helusio <103414789+Helusio@users.noreply.github.com>
2022-11-05 01:11:29 +0100

    reformatted gestis_data as csv, eliminated multiple values from formula column

--- URL_fetcher.py
+++ URL_fetcher.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 
-gestis_data = pd.read_excel('gestis_data.xlsx')
-print(gestis_data)
-print(gestis_data.loc[gestis_data['FORMULA'] == 'H2O'])
 
-
+gestis_data = pd.read_csv('gestis_data.csv')
+print(gestis_data.to_string())
