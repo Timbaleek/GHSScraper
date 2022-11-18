5791c94d0f3de4c5832b8ce2735bdd7bc6b9376f
Helusio <103414789+Helusio@users.noreply.github.com>
2022-11-18 15:03:08 +0100

    formatting to word and latex actually works now

--- HP_format.py
+++ HP_format.py
@@ -25,49 +25,107 @@ def format_for_word(cid, name, sort_alphabetically):
         hazards.sort()
         precautions.sort()
 
+    out.append(name)
+    out.append('')
     out.append('Hazard Statements')
-    out.append([])
+    out.append('')
 
-    for h in hazards:
+    for h, i in zip(precautions, range(len(hazards))):
 
         if '+' in h:
-            h.split('+')
+            hazards[i] = h.split('+')
 
-            for i in range(len(h)-1):
-                h[i+1] = '+' + h[i+1]
+            for j in range(len(hazards[i])-1):
+                hazards[i][j+1] = '+ ' + hazards[i][j+1]
 
     hazards = flatten(hazards)
 
     for h in hazards:
+        out.append((h + ':').ljust(10) + hazards_dict['phrase'].loc[h.replace('+', '')])
 
-        if '+' in h:
-            h.replace('+', '')
-            out.append((h + ':').ljust(10) + hazards_dict[h])
-            h = '+ ' + h
-        else:
-            out.append((h + ':').ljust(10) + hazards_dict[h])
+    out.append('')
+    out.append('Precautionary Statements')
+    out.append('')
 
-    out.append([])
-    out.append('Precationary Statements')
-    out.append([])
-
-    for p in precautions:
+    for p, i in zip(precautions, range(len(precautions))):
 
         if '+' in p:
-            p.split('+')
+            precautions[i] = p.split('+')
 
-            for i in range(len(p) - 1):
-                p[i + 1] = '+' + p[i + 1]
+            for j in range(len(precautions[i]) - 1):
+                precautions[i][j+1] = '+ ' + precautions[i][j+1]
 
     precautions = flatten(precautions)
 
     for p in precautions:
-
-        if '+' in p:
-            p.replace('+', '')
-            out.append((p + ':').ljust(10) + hazards_dict[p])
-            p = '+ ' + p
-        else:
-            out.append((p + ':').ljust(10) + hazards_dict[p])
+        out.append((p + ':').ljust(10) + precautions_dict['phrase'].loc[p.replace('+ ', '')])
 
     return out
+
+
+def format_for_latex(cid, name, sort_alphabetically):
+
+    hazards_dict = pd.read_excel('hazards_dict.xlsx', index_col=0)
+    precautions_dict = pd.read_excel('precautions_dict.xlsx', index_col=0)
+
+    out = []
+    pictograms = []
+    hazards, precautions = get_H_and_P(cid)
+
+    if sort_alphabetically:
+        hazards.sort()
+        precautions.sort()
+
+    out.append(r'\subsubsection*{name}')
+    out.append('')
+    out.append(r'\textbf{Hazard Statements}')
+    out.append(r'\bigskip')
+    out.append('')
+
+    for h, i in zip(precautions, range(len(hazards))):
+
+        if '+' in h:
+            hazards[i] = h.split('+')
+
+            for j in range(len(hazards[i]) - 1):
+                hazards[i][j + 1] = '+ ' + hazards[i][j + 1]
+
+    hazards = flatten(hazards)
+
+    for h in hazards:
+        if not hazards_dict['pictogram'].loc[h.replace('+ ', '')] in pictograms:
+            out.append(r'\ghspic{' + hazards_dict['pictogram'].loc[h.replace('+ ', '')] + r'}')
+            pictograms.append(hazards_dict['pictogram'].loc[h.replace('+ ', '')])
+
+    out.append(r'\begin{description}')
+    out.append('')
+    out.append(r'\itemsep -1.5mm')
+
+    for h in hazards:
+        out.append(r'\item{\textbf{' + (h + ':') + r'}}' + hazards_dict['phrase'].loc[h.replace('+ ', '')])
+
+    out.append('')
+    out.append(r'\end{description}')
+    out.append('')
+    out.append(r'\textbf{Precautionary Statements}')
+    out.append('')
+    out.append(r'\begin{description}')
+    out.append('')
+    out.append(r'\itemsep -1.5mm')
+    out.append('')
+
+    for p, i in zip(precautions, range(len(precautions))):
+
+        if '+' in p:
+            precautions[i] = p.split('+')
+
+            for j in range(len(precautions[i]) - 1):
+                precautions[i][j + 1] = '+ ' + precautions[i][j + 1]
+
+    precautions = flatten(precautions)
+
+    for p in precautions:
+        out.append(r'\item{\textbf{' + (p + ':') + r'}}' + precautions_dict['phrase'].loc[p.replace('+ ', '')])
+
+    return out
+
