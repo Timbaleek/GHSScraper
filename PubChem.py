a6b398c6836df1b00e0a12fee285868f8c91b6e4
Tim Appel <timbaleek@hotmail.de>
2022-12-05 08:32:19 +0100

    noindent before description

--- PubChem.py
+++ PubChem.py
@@ -26,14 +26,14 @@ def get_cid_from_name(name):
 
 def get_name_from_cid(cid):
     url = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/' + \
-        str(cid) + '/property/IUPACName/JSON'
+        str(cid) + '/props/IUPACName/JSON'
     r = requests.get(url)
-    return r.json().get('PropertyTable').get('Properties')[0].get('IUPACName')
+    return r.json().get('PropertyTable').get('Props')[0].get('IUPACName')
 
 
 def get_cas_from_cid(cid):
     url = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/' + \
-        str(cid) + '/property/CAS/JSON'
+        str(cid) + '/props/CAS/JSON'
     r = requests.get(url)
     return r.json().get('PropertyTable').get('Properties')[0].get('CAS')
 
@@ -41,16 +41,12 @@ def get_cas_from_cid(cid):
 # print(get_cas_from_cid(2244))
 
 
-'''
-def get_cid_from_cas(cas):
-    url = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cas/' + \
-        str(cas) + '/cids/JSON'
+# Get Display Name from CID
+def get_display_name_from_cid(cid):
+    url = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/' + \
+        str(cid) + '/JSON'
     r = requests.get(url)
-    print(r.json())
-    return 0  # r.json().get('IdentifierList').get('CAS')[0]
-
-get_cid_from_cas('64-17-5')
-'''
+    return r.json().get('PC_Compounds')[0].get('props')[6].get('value').get('sval')
 
 
 def get_H_and_P_from_name(name):
@@ -106,7 +102,7 @@ def get_H_and_P(cid):
 def to_latex_from_name(name):
     cid = get_cid_from_name(name)
     HandP = get_H_and_P(cid)
-    print("\\HPStatements{" + str(get_name_from_cid(cid)) +
+    print("\\HPStatements{" + str(get_display_name_from_cid(cid)) +
           "}{\\ghspic{exclam}\\\\\\\\}")
     to_latex(HandP)
 
@@ -130,3 +126,6 @@ def to_latex(HandP):
 
     print("\\end{itemize}}")
     print("{}")
+
+
+to_latex_from_name(input("Enter the name of the compound: "))
